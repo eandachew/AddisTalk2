a5f330ae78a07de69c3b7a948b6231587c54ba63
eandachew <eandachew@gmail.com>
2025-12-30 19:47:12 +0000

    generic post view

--- blog/models.py
+++ blog/models.py
@@ -23,3 +23,23 @@ class Post(models.Model):
     
     def __str__(self):
         return self.title
+    
+class Comment(models.Model):
+    """
+    Model representing a comment on a post.
+    """
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments"
+    )
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="commenter"
+    )
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ["created_on"]
+    
+    def __str__(self):
+        return f"Comment by {self.author} on {self.post.title}"
